5e1c7849eb2007a113084b22d8b00f59bacdddd4
Sulis Triyono <sulis.tryono@gmail.com>
2019-02-13 22:31:24 +0700

    Restructuring pug folder, updating packages, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,85 @@
 var gulp    = require('gulp'),
-    connect = require('gulp-connect'),
-    plumber = require('gulp-plumber'),
-    pug     = require('gulp-pug'),
-    rename  = require('gulp-rename'),
     sass    = require('gulp-sass'),
-    uglify  = require('gulp-uglify');
+    connect = require('gulp-connect'),
+    pug     = require('gulp-pug'),
+    plumber = require('gulp-plumber'),
+    rename  = require('gulp-rename'),
+    uglify  = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('connect', function() {
+function reload(done) {
   connect.server({
     livereload: true,
     port: 8080
   });
-});
+  done();
+}
 
-gulp.task('styles', function() {
-  gulp.src('src/sass/styles.scss')
-  .pipe(plumber())
-  .pipe(sass({outputStyle: 'compressed'}))
-  .pipe(rename('styles.min.css'))
-  .pipe(gulp.dest('assets/css'))
-  .pipe(connect.reload());
-});
+function styles() {
+  return (
+    gulp.src('src/sass/styles.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(connect.reload())
+  );
+}
 
-gulp.task('scripts', function() {
-  gulp.src('src/js/scripts.js')
-  .pipe(plumber())
-  .pipe(uglify())
-  .pipe(rename('scripts.min.js'))
-  .pipe(gulp.dest('assets/js'))
-  .pipe(connect.reload());
-});
+function scripts() {
+  return (
+    gulp.src('src/js/scripts.js')
+    .pipe(plumber())
+    .pipe(gulp.dest('assets/js'))
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(connect.reload())
+  );
+}
 
-gulp.task('html', function() {
-  gulp.src('*.html')
-  .pipe(plumber())
-  .pipe(connect.reload());
-});
+function html() {
+  return (
+    gulp.src('*.html')
+    .pipe(plumber())
+    .pipe(connect.reload())
+  );
+}
 
-gulp.task('views', function buildHTML() {
-  return gulp.src('src/pug/pages/*.pug')
-  .pipe(plumber())
-  .pipe(pug({
-      pretty: true
-  }))
-  .pipe(gulp.dest(''))
-  .pipe(connect.reload());
-});
+function views() {
+  return (
+    gulp.src('src/pug/pages/*.pug')
+    .pipe(plumber())
+    .pipe(pug({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(connect.reload())
+  )
+}
 
-gulp.task('watch', function () {
-  gulp.watch(['*.html'], ['html']);
-  gulp.watch(['src/sass/**/*.scss'], ['styles']);
-  gulp.watch(['src/js/scripts.js'], ['scripts']);
-  gulp.watch(['src/pug/**/*.pug'], ['views']);
-});
+function watchTask(done) {
+  gulp.watch('*.html', html);
+  gulp.watch('src/sass/**/*.scss', styles);
+  gulp.watch('src/js/scripts.js', scripts);
+  gulp.watch('src/pug/**/*.pug', views);
+  done();
+}
 
-gulp.task('default', ['connect', 'styles', 'scripts', 'html', 'views', 'watch']);
+const watch = gulp.parallel(watchTask, reload);
+const build = gulp.series(gulp.parallel(styles, scripts, html, views));
+
+exports.reload = reload;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.html = html;
+exports.views = views;
+exports.watch = watch;
+exports.build = build;
+exports.default = watch;
